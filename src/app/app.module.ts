4c6062bf0d9f0b5d2403859ceac7bcfa1cb880fa
Natan <natancunha@gmail.com>
2018-09-17 15:58:36 -0300

    config adicionais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,13 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { CardapioPage } from '../pages/cardapio/cardapio'
-import { MapaPage } from '../pages/mapa/mapa'
-import { ContatoPage } from '../pages/contato/contato'
+import { MapaPage } from '../pages/mapa/mapa';
+import { ContatoPage } from '../pages/contato/contato';
+import { CarnesPage } from '../pages/carnes/carnes';
+import { PeixesPage } from '../pages/peixes/peixes';
+import { SaladasPage } from '../pages/saladas/saladas';
+import { BebidasPage } from '../pages/bebidas/bebidas';
+import { SobremesasPage } from '../pages/sobremesas/sobremesas';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -23,7 +28,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage,
     CardapioPage,
     MapaPage,
-    ContatoPage
+    ContatoPage,
+    CarnesPage,
+    PeixesPage,
+    SaladasPage,
+    BebidasPage,
+    SobremesasPage
   ],
   imports: [
     BrowserModule,
@@ -38,7 +48,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage,
     CardapioPage,
     MapaPage,
-    ContatoPage
+    ContatoPage,
+    CarnesPage,
+    PeixesPage,
+    SaladasPage,
+    BebidasPage,
+    SobremesasPage
   ],
   providers: [
     StatusBar,

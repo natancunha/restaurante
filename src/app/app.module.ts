0c8320e13fe77bcb22a7609ac68a5b99c938dcad
Natan <natancunha@gmail.com>
2018-09-17 13:36:23 -0300

    pagina contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { CardapioPage } from '../pages/cardapio/cardapio'
+import { MapaPage } from '../pages/mapa/mapa'
+import { ContatoPage } from '../pages/contato/contato'
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,7 +20,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    CardapioPage,
+    MapaPage,
+    ContatoPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +35,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    CardapioPage,
+    MapaPage,
+    ContatoPage
   ],
   providers: [
     StatusBar,

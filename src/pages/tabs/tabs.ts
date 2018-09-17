db0c18d2196895ffa9b8aafc1e2ce726bd220611
Natan <natancunha@gmail.com>
2018-09-17 14:41:30 -0300

    mudancas visuais

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,15 +3,19 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { CardapioPage } from '../cardapio/cardapio'
+import { MapaPage } from '../mapa/mapa'
+import { ContatoPage } from '../contato/contato'
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = AboutPage;
-  tab2Root = HomePage;
-  tab3Root = ContactPage;
+  tab1Root = ContatoPage;
+  tab2Root = CardapioPage;
+  tab3Root = MapaPage;
 
   constructor() {
 
